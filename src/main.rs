45f677f6cddcde5a419fda83dc8917a07239cb61
Hang Wang <hang@onlyalt.com>
2021-12-19 17:49:02 -0500

    add 2nd task for day 1

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::fs;
 fn main() {
-    let filename = "../aoc_input1";
+    // let filename = "examples/day_01";
+    let filename = "inputs/day_01";
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
-    run1_1(&contents)
+    let res = run1_2(&contents);
+    println!("result is {:?}", res);
 }
 
-fn run1_1(contents: &String) {
+fn run1_1(contents: &String) -> u32{
     let mut curr:Option<u32> = None;
     let mut count = 0;
     for line in contents.lines() {
@@ -20,6 +22,41 @@ fn run1_1(contents: &String) {
             None => 0,
         };
         };
-    println!("count {:?}", count);
+    count
 }
 
+
+fn run1_2(contents: &String) -> u32{
+    let depths: Vec<u32> = contents 
+            .lines()
+            .map(|x| {
+                x.parse()
+                    .expect("could not parse invalid integer string")
+            })
+            .collect();
+    depths
+        .iter()
+        .fold(
+            (0, None, None, None),
+            |(count, a, b, c), x|{
+                match (a, b, c){
+                    (Some(aa), Some(_bb), Some(_cc)) => {
+                        if  x > aa {
+                            (count + 1, b, c, Some(x))
+                        }
+                        else{
+                            (count, b, c, Some(x))
+                        }
+
+                    },
+                    (None, None, None) => (count, None, None, Some(x)),
+                    (None, None, Some(_cc)) => (count, None, c, Some(x)),
+                    (None, Some(_bb), Some(_cc)) => (count, b, c, Some(x)),
+                    _ => panic!("logicerror"),
+
+                }
+
+            }
+            ).0
+            
+}
