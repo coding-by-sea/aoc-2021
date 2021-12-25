331eac0095bd50940f7e676f03517795d45f0f31
Hang Wang <hang@onlyalt.com>
2021-12-25 08:45:46 -0500

    added solutions for day 2 and inputs, examples for day 3

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::fs;
 fn main() {
-    // let filename = "examples/day_01";
-    let filename = "inputs/day_01";
+    // let filename = "examples/day_02";
+    let filename = "inputs/day_02";
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
-    let res = run1_2(&contents);
+    let res = run2_2(&contents);
     println!("result is {:?}", res);
 }
 
@@ -52,11 +52,60 @@ fn run1_2(contents: &String) -> u32{
                     (None, None, None) => (count, None, None, Some(x)),
                     (None, None, Some(_cc)) => (count, None, c, Some(x)),
                     (None, Some(_bb), Some(_cc)) => (count, b, c, Some(x)),
-                    _ => panic!("logicerror"),
+                    _ => panic!("logic error"),
 
                 }
 
             }
             ).0
             
+}
+
+#[derive(Debug)]
+struct SubmarineOperation{
+    direction: String,
+    steps: u32,
+}
+
+impl SubmarineOperation{
+    fn new(line: &str) -> SubmarineOperation {
+        let mut splited = line.split(' ');
+        let direction = splited.next().unwrap().to_string();
+        let steps = splited.next().unwrap().parse().expect("failed to parse into an integer");
+        SubmarineOperation { direction, steps }
+    }
+}
+
+fn run2_1(contents: &String) -> u32{
+        let operations = contents.lines().map(|line|SubmarineOperation::new(line)).collect::<Vec<SubmarineOperation>>();
+        operations
+            .iter()
+            .fold([0, 0], |[pos, dep], operation| {match operation.direction.as_str(){
+                "forward" => [pos + operation.steps, dep],
+                "backward" => [pos - operation.steps, dep],
+                "up" => [pos,  dep - operation.steps],
+                "down" => [pos,  dep + operation.steps],
+                _=>[pos, dep]
+            }})
+            .iter()
+            .fold(1, |res, x| {res * x})
+    }
+
+fn run2_2(contents: &String) -> u32 {
+    let operations = contents.lines().map(|line| SubmarineOperation::new(line)).collect::<Vec<SubmarineOperation>>();
+    let res = operations
+        .iter()
+        .fold([0, 0, 0], |[pos, dep, aim], operation| {
+            match operation.direction.as_str() {
+                "forward" => [pos + operation.steps, dep + operation.steps * aim, aim],
+                "up" => [pos, dep, aim - operation.steps],
+                "down" => [pos, dep, aim + operation.steps],
+                _ => [pos, dep, aim]
+            }
+        });
+    res.split_last()
+        .unwrap()
+        .1
+        .iter()
+        .fold(1, |res, x| { res * x })
 }
