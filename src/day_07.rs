f4fd2bb4833048cce04f7c15651955cae7c85628
Hang Wang <hang@onlyalt.com>
2022-01-22 21:31:13 -0500

    finish 07

---
+++ src/day_07.rs
@@ -0,0 +1,66 @@
+use std::cmp;
+
+#[derive(Debug)]
+struct Crab {
+    pos: i64
+}
+
+impl Crab {
+    fn get_constant_fuel_cost(&self, pos_x: i64) -> i64{
+        (self.pos - pos_x).abs()
+    }
+    fn get_increasing_fuel_cost(&self, pos_x: i64) -> i64{
+        (self.pos - pos_x).abs() * (1+ (self.pos - pos_x).abs()) / 2
+    }
+}
+impl From<&str> for Crab {
+    fn from(value: &str) -> Self {
+        Crab {
+            pos: value.parse().unwrap()
+        }
+    }
+}
+
+pub fn part_1(contents: &str) -> i64 {
+    let school:Vec<Crab> = contents.split(',').map(|x| Crab::from(x)).collect();
+    let min_pos: i64 = school.iter().fold(0, |min, crab| cmp::min(min, crab.pos));
+    let max_pos: i64 = school.iter().fold(0, |max, crab| cmp::max(max, crab.pos));
+    let mut left = min_pos;
+    let mut right = max_pos;
+    while left + 1 < right {
+        let mid = left + (right - left) / 2;
+        let fuel_cost_mid_minus_one = school.iter().fold(0, |acc, crab| acc + crab.get_constant_fuel_cost(mid - 1));
+        let fuel_cost_mid = school.iter().fold(0, |acc, crab| acc + crab.get_constant_fuel_cost(mid));
+        if fuel_cost_mid_minus_one >= fuel_cost_mid {
+            left = mid;
+        }
+        else {
+            right = mid;
+        }
+    }
+    let fuel_cost_left = school.iter().fold(0, |acc, crab| acc + crab.get_constant_fuel_cost(left));
+    let fuel_cost_right = school.iter().fold(0, |acc, crab| acc + crab.get_constant_fuel_cost(right));
+    cmp::min(fuel_cost_left, fuel_cost_right)
+}
+
+pub fn part_2(contents: &str) -> i64 {
+    let school:Vec<Crab> = contents.split(',').map(|x| Crab::from(x)).collect();
+    let min_pos: i64 = school.iter().fold(0, |min, crab| cmp::min(min, crab.pos));
+    let max_pos: i64 = school.iter().fold(0, |max, crab| cmp::max(max, crab.pos));
+    let mut left = min_pos;
+    let mut right = max_pos;
+    while left + 1 < right {
+        let mid = left + (right - left) / 2;
+        let fuel_cost_mid_minus_one = school.iter().fold(0, |acc, crab| acc + crab.get_increasing_fuel_cost(mid - 1));
+        let fuel_cost_mid = school.iter().fold(0, |acc, crab| acc + crab.get_increasing_fuel_cost(mid));
+        if fuel_cost_mid_minus_one >= fuel_cost_mid {
+            left = mid;
+        }
+        else {
+            right = mid;
+        }
+    }
+    let fuel_cost_left = school.iter().fold(0, |acc, crab| acc + crab.get_increasing_fuel_cost(left));
+    let fuel_cost_right = school.iter().fold(0, |acc, crab| acc + crab.get_increasing_fuel_cost(right));
+    cmp::min(fuel_cost_left, fuel_cost_right)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::fs;
-mod day_06;
-pub use crate::day_06::part_1;
-pub use crate::day_06::part_2;
+mod day_07;
+pub use crate::day_07::part_1;
+pub use crate::day_07::part_2;
 
 fn main() {
-    // let filename = "examples/day_06";
-    let filename = "inputs/day_06";
+    // let filename = "examples/day_07";
+    let filename = "inputs/day_07";
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
     let res = part_2(&contents);
     println!("result is {:?}", res);
